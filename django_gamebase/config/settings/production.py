b8a55b8f6e73d0a9bef16ac5fdacc8515343d4c6
Your Name <you@example.com>
2026-08-04 20:18:24 +0200

    add secure_proxy_ssl_header due to infinite 301s

--- django_gamebase/config/settings/production.py
+++ django_gamebase/config/settings/production.py
@@ -28,6 +28,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = (
+    "HTTP_X_FORWARDED_PROTO",
+    "https",
+)
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
